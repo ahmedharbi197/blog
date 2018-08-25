7a293d9ff9405e2d985ef756b64280a40ce40d92
Ahmed Harbi <ahmedharbi@ieee.org>
2018-08-25 03:22:06 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  title: 'Blog', // Required
+  author: 'Ahmed Harbi', // Required
+  tagline: 'Electronics and Communications Engineering Student',
+  primaryColor: '#438c6c', // Required
+  showHeaderImage: true,
+  showShareButtons: true,
+  postsPerPage: 5, // Required
+  social: {
+    website: 'https://ahmedharbi197.github.io/',
+    github: 'https://www.github.com/ahmedharbi197',
+    twitter: 'https://www.twitter.com/ahmedharbiii',
+    linkedin: 'https://www.linkedin.com/in/ahmed-harbi-1992aa166/'
+  }
+};
